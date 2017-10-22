8b68069e1d2bd375db6791a5229ba02d9d300e14
Ed Harper <edwardharper01@gmail.com>
2017-10-22 15:21:56 +0100

    Just try hidraw0 and 4

--- libre-wrapper.sh
+++ libre-wrapper.sh
@@ -2,12 +2,17 @@
 
 resultcode=1
 hidval=0
-while [ $resultcode -ne  0 ] && [ $hidval -lt 10 ]
+while [ $resultcode -ne  0 ] && [ $hidval -lt 5 ]
 do
     echo trying /dev/hidraw$hidval...
     python3 glucometer.py --driver=fslibre --device=/dev/hidraw$hidval dump
     resultcode=$?
-    let hidval=hidval+1
+    if (( hidval==0 ))
+    then
+        let hidval=4
+    else 
+        let hidval=hidval+1
+    fi
 done
 
 
